006c7f3c7d429dd4ae45b02b3a26f1f1250f582b
MD. LINGKON ISLAM <111384782+lingkon-record@users.noreply.github.com>
2023-11-21 00:30:21 +0600

    Create Filters-Digit-Count.js

---
+++ 003-javascript/Filters-Digit-Count.js
@@ -0,0 +1,8 @@
+ // Assuming you have a mixed list of ABC, DIGIT, and SYMBOL characters
+const mixedList = ['ABC', '123456', 'XYZ', '!@#$%', '987654', 'abc123', '1234567', '456789'];
+
+// Use the filter method to get only 6-digit numbers
+const sixDigitNumbers = mixedList.filter(item => /^\d{6}$/.test(item));
+
+// Log the result to the console
+console.log("6-digit Numbers:", sixDigitNumbers);
