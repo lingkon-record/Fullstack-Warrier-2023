9d96072f2304b7473690a0828656624dce617dc7
MD. LINGKON ISLAM <111384782+lingkon-record@users.noreply.github.com>
2023-12-12 00:42:04 +0600

    Create removeDuplicates.js

---
+++ 003-javascript/removeDuplicates.js
@@ -0,0 +1,11 @@
+ // Function to remove duplicates from an array
+function removeDuplicates(arr) {
+  return Array.from(new Set(arr));
+}
+
+// Example usage
+const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
+const arrayWithoutDuplicates = removeDuplicates(arrayWithDuplicates);
+
+console.log("Array with duplicates:", arrayWithDuplicates);
+console.log("Array without duplicates:", arrayWithoutDuplicates);
