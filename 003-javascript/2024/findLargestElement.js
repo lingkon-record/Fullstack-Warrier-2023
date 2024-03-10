c25255b418b75b67dd1398ca9e807ee5230573d5
MD. LINGKON ISLAM <111384782+lingkon-record@users.noreply.github.com>
2024-03-11 00:19:05 +0600

    Update findLargestElement.js

--- 003-javascript/2024/findLargestElement.js
+++ 003-javascript/2024/findLargestElement.js
@@ -1,5 +1,30 @@
 // Write a JavaScript program to find the largest element in a nested array. 
 
+
+function findLargestElement(nestedArray) { 
+
+  let largest = nestedArray[0][0]; 
+
+  for (let arr of nestedArray) { 
+
+    for (let num of arr) { 
+
+      if (num > largest) { 
+
+        largest = num; 
+
+      } 
+
+    } 
+
+  } 
+
+  return largest; 
+
+} 
+
+// 02 gpt
+
 function findLargestElement(nestedArray) {
   let largestElement = Number.NEGATIVE_INFINITY;
 
