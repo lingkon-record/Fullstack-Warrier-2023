22f9eb6d8d44729c57c55081b56ce8878cd6ebd1
MD. LINGKON ISLAM <111384782+lingkon-record@users.noreply.github.com>
2024-05-12 00:58:30 +0600

    Update 003-maritalStatuses.js

--- 003-javascript/Random/003-maritalStatuses.js
+++ 003-javascript/Random/003-maritalStatuses.js
@@ -1,22 +1,24 @@
-let maritalStatuses = ["single", "married", "divorced", "widowed"];
-let messages = [];
+var maritalStatuses = ["single", "married", "divorced", "widowed"];
+var message = "";
 
-for (let status of maritalStatuses) {
-  let message = "";
-
+for (var i = 0; i < maritalStatuses.length; i++) {
+  var status = maritalStatuses[i];
+  
   if (status === "single") {
     message = "She is single and ready to mingle!";
+    break;
   } else if (status === "married") {
     message = "She is happily married!";
+    break;
   } else if (status === "divorced") {
     message = "She is divorced and loving life!";
+    break;
   } else if (status === "widowed") {
     message = "She is widowed and cherishing memories!";
+    break;
   } else {
     message = "Her marital status is unknown.";
   }
-
-  messages.push(message);
 }
 
-console.log(messages);
+console.log(message);
