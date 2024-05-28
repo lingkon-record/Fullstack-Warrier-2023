08cf8ea9825beef26eab1afb1a19c593c8b67480
MD. LINGKON ISLAM <111384782+lingkon-record@users.noreply.github.com>
2024-05-28 18:30:31 +0600

    Update random-generate.js

--- 003-JavaScript2/random-generate.js
+++ 003-JavaScript2/random-generate.js
@@ -2,7 +2,7 @@ const oldQuotes = [
     "Always be curious and never stop learning.",
     "Find a career that aligns with your passions.",
     "Networking is key to building your career.",
-    // Add more 
+
 ];
 
 
@@ -11,7 +11,7 @@ function getRandomQuote(quotesArray) {
     return quotesArray[randomIndex];
 }
 
-// Get a random quote from the array
+
 const randomQuote = getRandomQuote(oldQuotes);
 
 
