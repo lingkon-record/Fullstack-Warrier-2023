af1d433314ab89a3a68c76bd6d88e7a2e318e852
MD. LINGKON ISLAM <111384782+lingkon-record@users.noreply.github.com>
2024-06-21 21:51:47 +0600

    Update Console-Obj-Arrays-02.js

--- 003-javascript/Console-Obj-Arrays-02.js
+++ 003-javascript/Console-Obj-Arrays-02.js
@@ -11,13 +11,13 @@ const data = [
   ]
 ];
 
-// Logging the entire data array
+// Logging -- the entire data array
 console.log('Data array:', data);
 
 // Accessing and logging specific elements
 console.log('First element:', data[0]);
 console.log('Fourth element:', data[3]);
 
-// Accessing and logging specific elements within nested arrays and objects
+// Accessing and logging specific elements -- within nested arrays and objects
 console.log('Third name:', data[2].name);
 console.log('Grape type:', data[3][3].type);
